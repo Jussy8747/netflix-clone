9a1387b29fac9aef03a324cea44fd76e3fa31057
Justice Duruanyim <uzochiduru8747@gmail.com>
2023-08-16 07:23:08 +0100

    made some changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-
 import { MainPageContextProvider } from './context/MainPageContext.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
